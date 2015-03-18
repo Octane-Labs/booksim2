03fb752f6ff606fc27fc9741db2defd71e6d18de
Sriram Damodharan <sriram@octanelabs.in>
2015-03-18 10:22:17 +0530

    ZMQ test stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,41 @@
 // $Id$
 
 /*
- Copyright (c) 2007-2012, Trustees of The Leland Stanford Junior University
- All rights reserved.
+Copyright (c) 2007-2012, Trustees of The Leland Stanford Junior University
+All rights reserved.
 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
- Redistributions of source code must retain the above copyright notice, this 
- list of conditions and the following disclaimer.
- Redistributions in binary form must reproduce the above copyright notice, this
- list of conditions and the following disclaimer in the documentation and/or
- other materials provided with the distribution.
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 /*main.cpp
- *
- *The starting point of the network simulator
- *-Include all network header files
- *-initilize the network
- *-initialize the traffic manager and set it to run
- *
- *
- */
+*
+*The starting point of the network simulator
+*-Include all network header files
+*-initilize the network
+*-initialize the traffic manager and set it to run
+*
+*
+*/
 #include <sys/time.h>
+#include <pthread.h>
 
 #include <string>
 #include <cstdlib>
@@ -60,7 +61,7 @@
 //Global declarations
 //////////////////////
 
- /* the current traffic manager instance */
+/* the current traffic manager instance */
 TrafficManager * trafficManager = NULL;
 
 int GetSimTime() {
@@ -90,7 +91,9 @@ bool gTrace;
 
 ostream * gWatchOut;
 
+int threadCount;
 
+char* gGPEndoint;
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -100,8 +103,8 @@ bool Simulate( BookSimConfig const & config )
 
   int subnets = config.GetInt("subnets");
   /*To include a new network, must register the network here
-   *add an else if statement with the name of the network
-   */
+  *add an else if statement with the name of the network
+  */
   net.resize(subnets);
   for (int i = 0; i < subnets; ++i) {
     ostringstream name;
@@ -110,14 +113,14 @@ bool Simulate( BookSimConfig const & config )
   }
 
   /*tcc and characterize are legacy
-   *not sure how to use them 
-   */
+  *not sure how to use them
+  */
 
   assert(trafficManager == NULL);
   trafficManager = TrafficManager::New( config, net ) ;
 
   /*Start the simulation run
-   */
+  */
 
   double total_time; /* Amount of time we've run */
   struct timeval start_time, end_time; /* Time before/after user code */
@@ -129,7 +132,7 @@ bool Simulate( BookSimConfig const & config )
 
   gettimeofday(&end_time, NULL);
   total_time = ((double)(end_time.tv_sec) + (double)(end_time.tv_usec)/1000000.0)
-            - ((double)(start_time.tv_sec) + (double)(start_time.tv_usec)/1000000.0);
+  - ((double)(start_time.tv_sec) + (double)(start_time.tv_usec)/1000000.0);
 
   cout<<"Total run time "<<total_time<<endl;
 
@@ -150,26 +153,22 @@ bool Simulate( BookSimConfig const & config )
   return result;
 }
 
-
-int main( int argc, char **argv )
-{
+int spawnBooksim(int argc, char **argv) {
 
   BookSimConfig config;
-
 
   if ( !ParseArgs( &config, argc, argv ) ) {
     cerr << "Usage: " << argv[0] << " configfile... [param=value...]" << endl;
     return 0;
- } 
+  }
 
-  
   /*initialize routing, traffic, injection functions
-   */
+  */
   InitializeRoutingMap( config );
 
   gPrintActivity = (config.GetInt("print_activity") > 0);
   gTrace = (config.GetInt("viewer_trace") > 0);
-  
+
   string watch_out_file = config.GetStr( "watch_out" );
   if(watch_out_file == "") {
     gWatchOut = NULL;
@@ -178,10 +177,22 @@ int main( int argc, char **argv )
   } else {
     gWatchOut = new ofstream(watch_out_file.c_str());
   }
-  
+
 
   /*configure and run the simulator
-   */
+  */
   bool result = Simulate( config );
   return result ? -1 : 0;
+
+}
+
+int main( int argc, char **argv )
+{
+  threadCount = 0;
+  zsock_t *rep = zsock_new_rep("tcp://*:9999");
+  char* req = zstr_recv(rep);
+  cout<<req;
+  zstr_free(req);
+  zsock_destroy(rep);
+
 }
